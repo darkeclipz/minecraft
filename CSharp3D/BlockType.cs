e341bdf6f85aef36da037a0bcfbb36c27ee7f5d4
darkeclipz <lars@rotgers.io>
2024-12-31 00:50:51 +0100

    Moved block def to BlockType.cs

--- CSharp3D/BlockType.cs
+++ CSharp3D/BlockType.cs
@@ -26,3 +26,29 @@ public enum BlockType : byte
     BigPlantTop = 21,
     BigPlantBottom = 22,
 }
+
+public partial struct Block
+{
+    public static bool IsSolid(BlockType type) => type switch
+    {
+        BlockType.Air => false,
+        BlockType.Flower1 => false,
+        BlockType.Flower2 => false,
+        BlockType.Flower3 => false,
+        BlockType.Flower4 => false,
+        BlockType.BigPlantBottom => false,
+        BlockType.BigPlantTop => false,
+        _ => true
+    };
+
+    public static bool IsPlant(BlockType type) => type switch
+    {
+        BlockType.Flower1 => true,
+        BlockType.Flower2 => true,
+        BlockType.Flower3 => true,
+        BlockType.Flower4 => true,
+        BlockType.BigPlantBottom => true,
+        BlockType.BigPlantTop => true,
+        _ => false
+    };
+}
